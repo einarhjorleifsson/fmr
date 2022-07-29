698529b21c6c1e213748bed7fdf206c2abdb6e28
einarhjorleifsson <einar.hjorleifsson@gmail.com>
2022-07-29 08:24:59 +0000

    connection established

--- R/ft_connect.R
+++ R/ft_connect.R
@@ -1,11 +1,12 @@
 # https://guillaumepressiat.github.io/blog/2019/11/oraclyr
 
-#' @title Connect to Fisheries Technology Database
+#' @title Connect to Fisheries Technology Mangager Oracle Database
 #'
 #' @description This function is the mother of all mothers that all other ft_ functions rely on. If not you are doomed.
 #'
 #' @param username your username
 #' @param password your password
+#' @param DBQ your path to the database
 #' @param use_odbc A boolean (default TRUE) controlling if connection via odbc or ROracle.
 #' The latter is generally faster but more difficult to setup.
 #'
@@ -13,37 +14,69 @@
 #'
 #' @export
 
-ft_connect <- function(username, password, use_odbc = TRUE) {
+ft_connect <- function(username, password, DBQ, use_odbc = TRUE) {
 
   if(use_odbc) {
-    con <- DBI::dbConnect(odbc::odbc(),
-                          UID = username,
-                          PWD = password,
-                          Driver = "Oracle",
-                          DBQ = "oracle.hav.fo/xe",
-                          Port = 1521,
-                          # SVC = "DB_SCHEMA", # schema when connection opens
-                          timeout = 10)
-    # con <- DBI::dbConnect(odbc::odbc(),
-    #                       UID = username,
-    #                       PWD = password,
-    #                       .connection_string = "Driver={Oracle};DBQ=oracle.hav.fo:1521/xe;",
-    #                       timeout = 10)
-  } else {
-
-    drv <- DBI::dbDriver("Oracle")
-    host <- "oracle.hav.fo"   # Needs to be replaced
-    port <- 1521
-    xe <- "xe"                # This is dbname
-
-    connect.string <- paste(
-      "(DESCRIPTION=",
-      "(ADDRESS=(PROTOCOL=tcp)(HOST=", host, ")(PORT=", port, "))",
-      "(CONNECT_DATA=(SERVICE_NAME=", xe, ")))", sep = "")
-
-    con <- ROracle::dbConnect(drv, username = username, password = password,
-                              dbname = connect.string)
+    con <-
+      DBI::dbConnect(odbc::odbc(),
+                     Driver = "Oracle",
+                     Port = 1521,
+                     UID = username,
+                     PWD = password,
+                     DBQ = DBQ,
+                     timeout = 20)
   }
 
   return(con)
+}
+
+
+#' @export
+ft_tbl <- function (con, tbl) {
+  if (packageVersion("dbplyr") == "1.4.4") {
+    x <- strsplit(tbl, "\\.") %>% unlist()
+  }
+  else {
+    x <- strsplit(tbl, "\\.") %>% unlist() %>% purrr::map_if(!grepl("\"",
+                                                                    .), toupper) %>% gsub("\"", "", .)
+  }
+  dplyr::tbl(con, dbplyr::in_schema(x[1], x[2])) %>% dplyr::select_all(tolower)
+}
+
+#' @title Get overview of all tables in FTM
+#'
+#' @export
+ft_tables <- function(con) {
+  ft_tbl(con, "sys.all_tables")
+}
+
+#' @title Get overview of all views in FTM
+#'
+#' @export
+ft_views <- function(con) {
+  ft_tbl(con, "sys.all_views")
+}
+
+#' @title Vessel table
+#'
+#' @oaram con A oracle connection
+#'
+#' @export
+ft_vessels <- function(con) {
+  ft_tbl(con, "datistica1.vessels") |>
+    dplyr::select(-c(image:image_content_type, created_by:updated_time))
+}
+
+
+
+#' @export
+ft_asfis <- function(con) {
+  ft_tbl(con, "datistica1.z_asfis") |>
+    dplyr::select(-c(stats_data, image_name, image_content_type, image))
+}
+
+#' @export
+ft_species <- function(con) {
+  ft_tbl(con, "datistica1.species") |>
+    dplyr::select(species_id, friendly_name, asfis_id)
 }
--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 
 <!-- README.md is generated from README.Rmd. Please edit that file -->
 
-# ftr
+# ftmr
 
 <!-- badges: start -->
 <!-- badges: end -->
 
-The goal of {ftr} is to:
+The goal of {ftmr} is to:
 
--   Establish R-connection to Fisheries Technology (FT) Oracle database
--   Provide convenient functions to FT tables and views
+-   Establish R-connection to Fisheries Technology Management (FTM)
+    Oracle database
+-   Provide convenient functions to FTM tables and views
 -   Provide Rmarkdown report templates for various standardized reports
 -   R-code examples for data analysis and graphical presentations
